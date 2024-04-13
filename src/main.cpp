2d6929f0a3300fd3972e2987fdca7ba74c48cfaf
mrdcvlsc <mrdcvlsc@gmail.com>
2024-04-13 21:15:53 +0800

    xor neural network for example (unfinished)

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,26 @@
+#include <ATen/ops/sigmoid.h>
 #include <iostream>
+#include <memory>
+
 #include <torch/torch.h>
 
+struct XorNet: torch::nn::Module {
+  XorNet() {
+    fc1 = register_module("fc1", torch::nn::Linear(2, 2));
+    fc2 = register_module("fc2", torch::nn::Linear(2, 1));
+  }
+
+  torch::Tensor forward(torch::Tensor x) {
+    x = torch::sigmoid(fc1->forward(x));
+    x = torch::sigmoid(fc2->forward(x));
+    return x;
+  }
+
+  torch::nn::Linear fc1{nullptr}, fc2{nullptr};
+};
+
 int main() {
-  torch::Tensor tensor = torch::rand({2, 3});
-  std::cout << tensor << std::endl;
+  auto xor_net = std::make_shared<XorNet>();
+
+  std::cout << "xor_net : \n" << xor_net << "\n\n";
 }
